93251358a6165c6a5170e06fc83ddd2b3d01339a
Lee Parker <lee@klparker.com>
2011-02-21 22:44:22 -0600

    Upgrade to ZendFramework 1.11.3

--- library/Zend/Log/Formatter/Abstract.php
+++ library/Zend/Log/Formatter/Abstract.php
@@ -13,26 +13,28 @@
  * to license@zend.com so we can send you a copy immediately.
  *
  * @category   Zend
- * @package    Zend_Cloud
- * @subpackage StorageService
+ * @package    Zend_Log
+ * @subpackage Formatter
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @version    $Id: Abstract.php 23648 2011-01-21 19:04:20Z intiilapa $
  */
 
+/** @see Zend_Log_Formatter_Interface */
+require_once 'Zend/Log/Formatter/Interface.php';
 
-/**
- * Zend_Cloud_Exception
- */
-require_once 'Zend/Cloud/Exception.php';
-
+/** @see Zend_Log_FactoryInterface */
+require_once 'Zend/Log/FactoryInterface.php';
 
 /**
  * @category   Zend
- * @package    Zend_Cloud
- * @subpackage StorageService
+ * @package    Zend_Log
+ * @subpackage Formatter
  * @copyright  Copyright (c) 2005-2010 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
+ * @version    $Id: Abstract.php 23648 2011-01-21 19:04:20Z intiilapa $
  */
-class Zend_Cloud_StorageService_Exception extends Zend_Cloud_Exception
-{}
-
+abstract class Zend_Log_Formatter_Abstract
+    implements Zend_Log_Formatter_Interface, Zend_Log_FactoryInterface
+{
+}
