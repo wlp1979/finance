66af9340f87c959cf59a2e9d43d41aba8b60fa0b
Lee Parker <lee@klparker.com>
2015-07-14 09:20:02 -0500

    upgrade to newer versions of angularjs and bootsrap

--- web/app/js/app.js
+++ web/app/js/app.js
@@ -3,10 +3,22 @@
 (function() {
 	var app = angular.module('financeDirector', ['ngRoute', 'financeDirectorControllers', 'financeDirectorServices']);
 
+	app.factory('httpInterceptor', ['$q', '$window', '$location', function httpInterceptor ($q, $window, $location) {
+		return {
+			"responseError": function (response) {
+				if (response.status === 401) {
+					$location.url('/login');
+				}
+
+				return $q.reject(response);
+			}
+		};
+	}]);
+
 	app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
 		var defaultRoute = "/summary";
 
-		$httpProvider.responseInterceptors.push('httpInterceptor');
+		$httpProvider.interceptors.push('httpInterceptor');
 		$routeProvider.when('/summary', {
 			templateUrl: 'partials/balance-summary.html',
 			controller: 'BalanceSummaryCtrl',
@@ -25,23 +37,5 @@
 		otherwise({
 			redirectTo: defaultRoute
 		});
-	}]);
-
-	app.factory('httpInterceptor', ['$q', '$window', '$location', function httpInterceptor ($q, $window, $location) {
-		return function (promise) {
-			var success = function (response) {
-				return response;
-			};
-
-			var error = function (response) {
-				if (response.status === 401) {
-					$location.url('/login');
-				}
-
-				return $q.reject(response);
-			};
-
-			return promise.then(success, error);
-		};
 	}]);
 })();
